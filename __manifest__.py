3c58c84bd3009cbaa9505448a29745ed3d73d621
Santiago Bacale Ortiz <santiagobacale@gmail.com>
2021-04-12 13:04:23 +0200

    Nueva prueba para ver que pasa

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*- 
+
+{
+    'name': 'Servicio',
+    'summary': """Este módulo nos permitirá gestionar la asignación de servicios""",
+    'version': '13.0.0.0.0',
+    'description': """Este módulo nos permitirá gestionar la asignación de servicios""",
+    'author': 'Santiago Bacale Ortiz',
+    'company': 'Practicas Ultimo CAmbio',
+    'website': ' ',
+    'category': 'Tools',
+    'depends': ['base'],
+    'license': 'AGPL-3',
+    'data': [
+        'views/view.xml', 'security/ir.model.access.csv',
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+}
